6bcc3de1c6b8b7226befac1caeb274190f7d9054
mymyname <1264673507@qq.com>
2023-09-07 12:27:41 +0800

    axios标签请求

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,5 +1,9 @@
-import { defineComponent, onMounted, PropType, ref } from 'vue';
+import { AxiosError } from 'axios';
+import { Dialog } from 'vant';
+import { defineComponent, onMounted, PropType, reactive, ref } from 'vue';
+import { useRouter } from 'vue-router';
 import { MainLayout } from '../../layouts/MainLayout';
+import { BackIcon } from '../../shared/BackIcon';
 import { Button } from '../../shared/Button';
 import { http } from '../../shared/Http';
 import { Icon } from '../../shared/Icon';
@@ -8,45 +12,71 @@ import { useTags } from '../../shared/useTags';
 import { InputPad } from './InputPad';
 import s from './ItemCreate.module.scss';
 import { Tags } from './Tags';
-import { BackIcon } from '../../shared/BackIcon';
 export const ItemCreate = defineComponent({
   props: {
     name: {
-      type: String as PropType<string>
-    }
+      type: String as PropType<string>,
+    },
   },
   setup: (props, context) => {
-    const refKind = ref('支出')
-    const { tags: incomeTags,
-      hasMore: hasMore2,
-      fetchTags: fetchTags2
-    } = useTags((page) => {
-      return http.get<Resources<Tag>>('/tags', {
-        kind: 'income',
-        page: page + 1,
-        _mock: 'tagIndex'
-      })
-    })
+    const formData = reactive({
+      kind: '支出',
+      tags_id: [],
+      amount: 0,
+      happen_at: new Date().toISOString(),
+    });
+    const router = useRouter();
+    const onError = (error: AxiosError<ResourceError>) => {
+      if (error.response?.status === 422) {
+        Dialog.alert({
+          title: '出错',
+          message: Object.values(error.response.data.errors).join('\n'),
+        });
+      }
+      throw error;
+    };
+    const onSubmit = async () => {
+      await http
+        .post<Resource<Item>>('/items', formData, {
+          params: { _mock: 'itemCreate' },
+        })
+        .catch(onError);
+      router.push('/items');
+    };
     return () => (
-      <MainLayout class={s.layout}>{{
-        title: () => '记一笔',
-        icon: () => <BackIcon />,
-        default: () => <>
-          <div class={s.wrapper}>
-            <Tabs v-model:selected={refKind.value} class={s.tabs}>
-              <Tab name="支出">
-                <Tags kind="expenses"/>
-              </Tab>
-              <Tab name="收入">
-                <Tags kind="income"/>
-              </Tab>
-            </Tabs>
-            <div class={s.inputPad_wrapper}>
-              <InputPad />
-            </div>
-          </div>
-        </>
-      }}</MainLayout>
-    )
-  }
-})
+      <MainLayout class={s.layout}>
+        {{
+          title: () => '记一笔',
+          icon: () => <BackIcon />,
+          default: () => (
+            <>
+              <div class={s.wrapper}>
+                <Tabs v-model:selected={formData.kind} class={s.tabs}>
+                  <Tab name="支出">
+                    <Tags
+                      kind="expenses"
+                      v-model:selected={formData.tags_id[0]}
+                    />
+                  </Tab>
+                  <Tab name="收入">
+                    <Tags
+                      kind="income"
+                      v-model:selected={formData.tags_id[0]}
+                    />
+                  </Tab>
+                </Tabs>
+                <div class={s.inputPad_wrapper}>
+                  <InputPad
+                    v-model:happenAt={formData.happen_at}
+                    v-model:amount={formData.amount}
+                    onSubmit={onSubmit}
+                  />
+                </div>
+              </div>
+            </>
+          ),
+        }}
+      </MainLayout>
+    );
+  },
+});
