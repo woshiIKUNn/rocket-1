1d5924a76e088ddf4fbc9aa7926ec340809029fe
mymyname <1264673507@qq.com>
2023-09-11 21:36:07 +0800

    实现记账页面

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -5,6 +5,13 @@ type Mock = (config: AxiosRequestConfig) => [number, any]
 
 faker.setLocale('zh_CN');
 
+export const mockItemIndexBalance: Mock = config => {
+  return [200, {
+    expenses: 9900,
+    income: 9900,
+    balance: 0
+  }]
+}
 export const mockItemIndex: Mock = (config) => {
   const { kind, page } = config.params
   const per_page = 25
@@ -14,18 +21,32 @@ export const mockItemIndex: Mock = (config) => {
     per_page,
     count,
   })
+  const createTag = (attrs?: any) =>
+    ({
+      id: createId(),
+      name: faker.lorem.word(),
+      sign: faker.internet.emoji(),
+      kind: 'expenses',
+      ...attrs
+    })
   const createItem = (n = 1, attrs?: any) =>
     Array.from({ length: n }).map(() => ({
       id: createId(),
       user_id: createId(),
       amount: Math.floor(Math.random() * 10000),
       tags_id: [createId()],
+      tags: [createTag()],
       happen_at: faker.date.past().toISOString(),
       kind: config.params.kind,
     }))
   const createBody = (n = 1, attrs?: any) => ({
     resources: createItem(n),
     pager: createPaper(page),
+    summary: {
+      income: 9900,
+      expenses: 9900,
+      balance: 0
+    }
   })
   if (!page || page === 1) {
     return [200, createBody(25)]
@@ -115,4 +136,3 @@ export const mockTagIndex: Mock = (config) => {
   }
 
 }
-
