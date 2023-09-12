ada9f3669c2181afc7986636a5980955fd6b991a
mymyname <1264673507@qq.com>
2023-09-12 15:13:28 +0800

    每次切换tab时重新渲染图表

--- src/shared/Tabs.tsx
+++ src/shared/Tabs.tsx
@@ -9,6 +9,10 @@ export const Tabs = defineComponent({
       type: String as PropType<string>,
       required: false,
     },
+    rerenderOnSelect: {
+      type: Boolean as PropType<boolean>,
+      default: false
+    }
   },
   emits: ['update:selected'],
   setup: (props, context) => {
@@ -33,16 +37,21 @@ export const Tabs = defineComponent({
               {item.props?.name}
             </li>)}
         </ol>
-        <div>
-        {tabs.map(item =>
-            <div v-show={item.props?.name === props.selected}>{item}</div>
-          )}
-        </div>
+
+        {props.rerenderOnSelect ?
+          <div key={props.selected}>
+            {tabs.find(item => item.props?.name === props.selected)}
+          </div> :
+          <div>
+            {tabs.map(item =>
+              <div v-show={item.props?.name === props.selected}>{item}</div>
+            )}
+          </div>
+        }
       </div>
     }
   }
 })
-
 export const Tab = defineComponent({
   props: {
     name: {
