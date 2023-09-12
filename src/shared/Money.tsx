9a1d5af7e59454e2c5fb5759bbe6ce32db2501b7
mymyname <1264673507@qq.com>
2023-09-12 16:29:00 +0800

    初步转换折线图数据

--- src/shared/Money.tsx
+++ src/shared/Money.tsx
@@ -22,3 +22,17 @@ export const Money = defineComponent({
   },
 })
 //to.Fixed(2)
+const addZero = (n: number) => {
+  const nString = n.toString()
+  const dotIndex = nString.indexOf('.')
+  if (dotIndex < 0) {
+    return nString + '.00'
+  } else if (nString.substring(dotIndex).length === 2) {
+    return nString + '0'
+  } else {
+    return nString
+  }
+}
+export const getMoney = (n: number) => {
+  return addZero(n / 100)
+}
